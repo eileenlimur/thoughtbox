c33b4601a0376c0d58a8ff67b590968ef2013a91
Eileen Li <eileenlimur@gmail.com>
2020-08-23 15:24:30 -0400

    configure server.js to client/build path and static index.html from build folder if process.env.NODE_ENV === 'production'

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,14 @@ app.post('/api/thoughts', (req, res) => {
   res.send(
     `I received your POST request. You sent me ${req.body.thought}`
   );
-  
+
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(___dirname, 'client/build')));
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`listening to port ${port}`));
