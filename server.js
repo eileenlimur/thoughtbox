355737ffed12a7fa3c1ec729aaeceb3fc0679109
Eileen Li <eileenlimur@gmail.com>
2020-08-23 15:43:22 -0400

    add secure = false to proxy configuration in package.json

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
 app.post('/api/thoughts', (req, res) => {
-  res.send(
-    `I received your POST request. You sent me ${req.body.thought}`
-  );
-
-});
+  const { thought } = req.body;
+  pool.query('INSERT INTO thoughts (thought) VALUES ($1);', [thought], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`Thought added`);
+  })
+})
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(___dirname, 'client/build')));
