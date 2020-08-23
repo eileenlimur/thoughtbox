5fc49223144a5cd8376ed2be9024ad31bf92b29c
Eileen Li <eileenlimur@gmail.com>
2020-08-23 14:34:45 -0400

    add all formerly created and styled components from thoughtbox1

---
+++ client/src/components.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+export function Nav(props) {
+  return (
+    <div className="header">
+      <h1 className="header_text">Thought</h1>&nbsp;&nbsp;&nbsp;
+      <h1 className="header_text">Box</h1>
+    </div>
+  );
+}
+
+export function Form(props) {
+  const [thought, setThought] = useState("");
+  const [mode, setMode] = useState("input");
+
+  function handleSubmit() {
+    
+    setMode("back to home");
+  }
+
+  function handleReject() {
+    setThought('');
+    setMode("input");
+  }
+
+  return (
+    <div>
+      <form className="form">
+        {mode !== "back to home" && (
+          <>
+            <h2>
+              Give me your <span>thoughts</span>.
+            </h2>
+            <input
+              type="text"
+              value={thought}
+              className="text-input"
+              onChange={(e) => {
+                if (mode === "input") {
+                  setThought(e.target.value);
+                }
+              }}
+              maxLength={1200}
+            />
+          </>
+        )}
+        {mode === "input" && (
+          <>
+            <h3>{1200 - thought.length} characters remaining</h3>
+            <button id="submit-button" onClick={handleSubmit}>
+              Submit
+            </button>
+          </>
+        )}
+        {mode === "back to home" && (
+          <>
+            <button className="view-thoughts-button" onClick={handleReject}>
+              Go back home
+            </button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
