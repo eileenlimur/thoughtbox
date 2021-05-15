43bc7704032d3a4e524e1f7a8ceebc58c24f20a9
Eileen Li <eileen@humancontact.com>
2021-05-15 19:43:17 -0400

    add id to SELECT

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/api/thoughts', (req, res) => {
 });
 
 app.get('/api/grieveyard', (req, res) => {
-  pool.query('SELECT title, content FROM grievestories ORDER BY id desc;', [], (err, pool_res) => {
+  pool.query('SELECT id, title, content FROM grievestories ORDER BY id desc;', [], (err, pool_res) => {
     if (err) {
       res.status(300).send(`Something went wrong with your database request`);
     } else {
