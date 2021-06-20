cea8fe865005c41a3f185bc0eee6fdfe68f1335c
Eileen Li <eileen@humancontact.com>
2021-06-20 19:03:39 -0400

    add author category

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/api/thoughts', (req, res) => {
 });
 
 app.get('/api/grieveyard', (req, res) => {
-  pool.query('SELECT id, title, content FROM grievestories ORDER BY id desc;', [], (err, pool_res) => {
+  pool.query('SELECT id, title, content, author FROM grievestories ORDER BY id desc;', [], (err, pool_res) => {
     if (err) {
       res.status(300).send(`Something went wrong with your database request`);
     } else {
